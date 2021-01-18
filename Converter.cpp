4d1ac9f617a04070c5d9db7147a2f5fa7c31f5ab
Thomas Griebel <thomas.griebel@live.com>
2021-01-18 12:10:08 -0600

    Binary model format
    geometry, tetures, and materials are all packed together for now

--- Converter.cpp
+++ Converter.cpp
@@ -16,7 +16,7 @@
 
 static const std::string ModelPath = "models/";
 static const std::string TexturePath = "textures/";
-static const std::string ConvertedPath = "converted/";
+static const std::string ConvertedPath = "models/";
 
 ResourceManager rm;
 
@@ -26,6 +26,7 @@ enum imageFormat_t
     IMAGE_FORMAT_PNG,
     IMAGE_FORMAT_BIN,
 };
+
 
 void ConvertImage( const std::string& srcFileName, const std::string& dstFileName, imageFormat_t format )
 {
@@ -55,6 +56,39 @@ void ConvertImage( const std::string& srcFileName, const std::string& dstFileNam
     stbi_image_free( pixels );
 }
 
+
+void LoadImage( const std::string& path, Image<Color>& outImage )
+{
+    int32_t width;
+    int32_t height;
+    int32_t channels;
+    stbi_uc* pixels = stbi_load( path.c_str(), &width, &height, &channels, STBI_rgb_alpha );
+    int32_t imageSize = width * height * 4;
+
+    if ( !pixels )
+    {
+        throw std::runtime_error( "Failed to load texture image!" );
+    }
+
+    outImage = Image<Color>( width, height );
+    for ( int32_t y = 0; y < height; ++y )
+    {
+        for ( int32_t x = 0; x < width; ++x )
+        {
+            Pixel pixel;
+            pixel.rgba.r = pixels[ ( y * width + x ) * 4 + 0 ];
+            pixel.rgba.g = pixels[ ( y * width + x ) * 4 + 1 ];
+            pixel.rgba.b = pixels[ ( y * width + x ) * 4 + 2 ];
+            pixel.rgba.a = pixels[ ( y * width + x ) * 4 + 3 ];
+
+            outImage.SetPixel( x, y, Color( pixel.r8g8b8a8 ) );
+        }
+    }
+
+    stbi_image_free( pixels );
+}
+
+
 uint32_t LoadModel( const std::string& path, ResourceManager& rm )
 {
     tinyobj::attrib_t attrib;
@@ -77,27 +111,38 @@ uint32_t LoadModel( const std::string& path, ResourceManager& rm )
     std::vector<uint32_t> indices;
     std::vector<vertex_t> uniqueVertices{};
 
+    const uint32_t vertexCount = attrib.vertices.size();
+    const uint32_t normalCount = attrib.normals.size();
+    const uint32_t textureCount = attrib.texcoords.size();
+
     for ( const auto& shape : shapes ) {
         for ( const auto& index : shape.mesh.indices )
         {
             vertex_t vert;
 
-            vert.pos[ 0 ] = attrib.vertices[ 3 * index.vertex_index + 0 ];
-            vert.pos[ 1 ] = attrib.vertices[ 3 * index.vertex_index + 1 ],
-            vert.pos[ 2 ] = attrib.vertices[ 3 * index.vertex_index + 2 ];
-
-            if ( index.normal_index != -1 )
+            if( ( index.vertex_index >= 0 ) && ( ( 3 * index.vertex_index + 2 ) < vertexCount ) )
             {
-                vert.normal[ 0 ] = attrib.vertices[ 3 * index.normal_index + 0 ];
-                vert.normal[ 1 ] = attrib.vertices[ 3 * index.normal_index + 1 ],
-                vert.normal[ 2 ] = attrib.vertices[ 3 * index.normal_index + 2 ];
+                vert.pos[ 0 ] = attrib.vertices[ 3 * index.vertex_index + 0 ];
+                vert.pos[ 1 ] = attrib.vertices[ 3 * index.vertex_index + 1 ],
+                vert.pos[ 2 ] = attrib.vertices[ 3 * index.vertex_index + 2 ];
+            }
+            else
+            {
+                vert.pos = vec4d( 0.0, 0.0, 0.0 );
+            }
+
+            if ( ( index.normal_index >= 0 ) && ( ( 3 * index.normal_index + 2 ) < normalCount ) )
+            {
+                vert.normal[ 0 ] = attrib.normals[ 3 * index.normal_index + 0 ];
+                vert.normal[ 1 ] = attrib.normals[ 3 * index.normal_index + 1 ],
+                vert.normal[ 2 ] = attrib.normals[ 3 * index.normal_index + 2 ];
             }
             else
             {
                 vert.normal = vec3d( 1.0, 0.0, 0.0 ).Normalize();
             }
 
-            if ( index.texcoord_index != -1 )
+            if ( ( index.texcoord_index >= 0 ) && ( ( 2 * index.texcoord_index + 1 ) < textureCount ) )
             {
                 vert.uv[ 0 ] = attrib.texcoords[ 2 * index.texcoord_index + 0 ];
                 vert.uv[ 1 ] = attrib.texcoords[ 2 * index.texcoord_index + 1 ];
@@ -163,35 +208,69 @@ uint32_t LoadModel( const std::string& path, ResourceManager& rm )
 
     const uint32_t modelIx = rm.AllocModel();
     Model* model = rm.GetModel( modelIx );
+    model->surfs.push_back( surface_t() );
 
     // Build VB and IB
     {
         model->name = path;
-        model->vb = rm.GetVB();
-        model->ib = rm.GetIB();
-        model->vbOffset = rm.GetVbOffset();
-        model->ibOffset = rm.GetIbOffset();
+        model->surfs.push_back( surface_t() );
+        model->surfs[ 0 ].vb = rm.GetVB();
+        model->surfs[ 0 ].ib = rm.GetIB();
+        model->surfs[ 0 ].vbOffset = rm.GetVbOffset();
+        model->surfs[ 0 ].ibOffset = rm.GetIbOffset();
 
         const uint32_t vertexCnt = uniqueVertices.size();
         for ( int32_t i = 0; i < vertexCnt; ++i )
         {
             rm.AddVertex( uniqueVertices[ i ] );
         }
-        model->vbEnd = rm.GetVbOffset();
+        model->surfs[ 0 ].vbEnd = rm.GetVbOffset();
 
         const size_t indexCnt = indices.size();
         assert( ( indexCnt % 3 ) == 0 );
         for ( size_t i = 0; i < indexCnt; i++ )
         {
-            rm.AddIndex( model->vbOffset + indices[ i ] );
+            rm.AddIndex( model->surfs[ 0 ].vbOffset + indices[ i ] );
         }
-        model->ibEnd = rm.GetIbOffset();
+        model->surfs[ 0 ].ibEnd = rm.GetIbOffset();
     }
 
-    std::string basePath = path.substr( 0, path.find_last_of( '/' ) );
+    if( materials.size() > 0  )
+    {
+        tinyobj::material_t& material = materials[ 0 ];
+        material_t& m = model->materials[ 0 ];
 
-    LoadMaterialObj( basePath + "/" + materialName, rm, model->material );
+        memset( m.name, 0, material_t::BufferSize );
+        strcpy_s( m.name, material_t::BufferSize, material.name.c_str() );
+        m.Ni = material.ior;
+        m.Ns = material.shininess;
+        m.Ka = material.ambient[ 0 ];
+        m.Ke = material.emission[ 0 ];
+        m.Kd = material.diffuse[ 0 ];
+        m.Ks = material.shininess;
+        m.Tf = material.transmittance[ 0 ];
+        m.Tr = 1.0 - Saturate( material.dissolve );
+        m.d = material.dissolve;
+        m.illum = material.illum;
+        m.textured = false;
 
+        model->materialCount += 1;
+
+        std::string name = path.substr( path.find_last_of( '/' ) + 1, path.size() );
+
+        if ( material.diffuse_texname.size() > 0 )
+        {
+            Image<Color> image;
+            LoadImage( std::string( TexturePath + material.diffuse_texname ), image );
+
+            Bitmap bitmap = Bitmap( image.GetWidth(), image.GetHeight() );
+            ImageToBitmap( image, bitmap );
+            bitmap.Write( "testConvert.bmp" );
+
+            m.colorMapId = rm.StoreImageCopy( image );
+            m.textured = true;
+        }
+    }
 
     return modelIx;
 }
@@ -199,8 +278,7 @@ uint32_t LoadModel( const std::string& path, ResourceManager& rm )
 
 int main()
 { 
-//  LoadModel( "2-Gears-LOW.obj" );
-//  ConvertImage( "EngineA_diff.png", "EngineA_diff", IMAGE_FORMAT_PNG );
+    ConvertImage( "tex.bmp", "test", IMAGE_FORMAT_BMP );
 
     uint32_t vb = rm.AllocVB();
     uint32_t ib = rm.AllocIB();
@@ -208,7 +286,7 @@ int main()
     rm.PushVB( vb );
     rm.PushIB( ib );
 
-    std::string modelName = "2-Gears-LOW";
+    std::string modelName = "12140_Skull_v3_L2";
 
     uint32_t srcModelId = LoadModel( ModelPath + modelName + ".obj", rm );
 
@@ -217,7 +295,8 @@ int main()
 
     Model* model = rm.GetModel( modelIx );
     std::string basePath = ModelPath.substr( 0, ModelPath.find_last_of( '/' ) );
-    LoadMaterialObj( basePath + "/" + model->material.name, rm, model->material );
+    LoadMaterialObj( basePath + "/" + model->materials[ 0 ].name, rm, model->materials[ 0 ] );
+    model->materialCount += 1;
 
     StoreModelObj( "test.obj", rm, modelIx );
 }
